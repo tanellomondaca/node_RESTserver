b60aaa1dae9672438965934dc1fa7d41d5381ad2
Tanelo <tanello.mondaca@gmail.com>
2022-07-12 11:54:43 -0400

    120. POST: creando usuario en la colección

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,49 +1,58 @@
-const { response, request } = require('express');
+const { response, request } = require("express");
+const Usuario = require("../models/usuario");
 
 const usuariosGet = (req = request, res = response) => {
     const query = req.query;
 
     res.json({
         msg: "Get desde controllers API",
-        query
+        query,
     });
-}
-    
-const usuariosPost = (req, res = response) => {
+};
+
+const usuariosPost = async (req, res = response) => {
     const body = req.body;
-    
+    const usuario = new Usuario(body); //nueva instancia de usuario que recibe y asigna los datos al modelo
+
+    await usuario
+        .save()
+        .then( () => console.log("Usuario grabado exitosamente."))
+        .catch((error) => {
+            console.log("Algo no salió bien");
+            console.log(error);
+        });
+
     res.json({
         msg: "POST desde controllers API",
-        body
+        usuario,
     });
-}
-    
+};
+
 const usuariosPut = (req, res = response) => {
     const id = req.params.id;
 
     res.json({
         msg: "PUT  desde controllers API",
-        id
+        id,
     });
-}
-    
+};
+
 const usuariosDelete = (req, res = response) => {
     res.json({
         msg: "DELETE desde controllers API",
     });
-}
-    
+};
+
 const usuariosPatch = (req, res = response) => {
     res.json({
         msg: "PATCH desde controllers API",
     });
-}
-    
+};
 
 module.exports = {
     usuariosGet,
     usuariosDelete,
     usuariosPatch,
     usuariosPost,
-    usuariosPut
-}
+    usuariosPut,
+};
