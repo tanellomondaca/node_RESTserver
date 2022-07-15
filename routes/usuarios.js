43dfa0132d3ee6b8a587db3fd8c5e842c944f87b
Tanelo <tanello.mondaca@gmail.com>
2022-07-15 15:43:50 -0400

    148. optimizar importaciones en node

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -2,9 +2,7 @@ const { Router } = require("express");
 const { check } = require("express-validator");
 
 // Middlewares
-const { validarCampos } = require('../middlewares/validar-campos');
-const { validarJWT } = require("../middlewares/validar-jwt");
-const { esAdminRole, tieneRole } = require("../middlewares/validar-roles");
+const {validarCampos, validarJWT, esAdminRole, tieneRole} = require('../middlewares');
 
 const {
     usuariosGet,
@@ -14,7 +12,11 @@ const {
     usuariosPatch,
 } = require("../controllers/usuarios-controllers");
 
-const { esRoleValido, emailExiste, existeUsuarioPorId } = require("../helpers/db-validators");
+const {
+    esRoleValido,
+    emailExiste,
+    existeUsuarioPorId,
+} = require("../helpers/db-validators");
 
 const router = Router();
 
@@ -23,33 +25,48 @@ const router = Router();
 //GET
 router.get("/", usuariosGet);
 //PUT
-router.put("/:id",[
-    check('id', 'No es un ID válido').isMongoId(),
-    check('id').custom( existeUsuarioPorId ),
-    check('rol').custom( esRoleValido ),
-    validarCampos
-], usuariosPut);
+router.put(
+    "/:id",
+    [
+        check("id", "No es un ID válido").isMongoId(),
+        check("id").custom(existeUsuarioPorId),
+        check("rol").custom(esRoleValido),
+        validarCampos,
+    ],
+    usuariosPut
+);
 
 //POST
-router.post("/",[ // Middlewares
-    check('correo', 'El correo no es válido.').isEmail(),
-    check('correo').custom(emailExiste),
-    check('nombre', 'El nombre es obligatorio.').not().isEmpty(),
-    check('password', 'El password debe ser de más de 6 letras.').isLength({min: 6}),
-    // check('rol', 'No es un rol válido').isIn(['ADMIN_ROLE','USER_ROLE']),
-    check('rol').custom( esRoleValido ),
-    validarCampos
-] , usuariosPost);
+router.post(
+    "/",
+    [
+        // Middlewares
+        check("correo", "El correo no es válido.").isEmail(),
+        check("correo").custom(emailExiste),
+        check("nombre", "El nombre es obligatorio.").not().isEmpty(),
+        check("password", "El password debe ser de más de 6 letras.").isLength({
+            min: 6,
+        }),
+        // check('rol', 'No es un rol válido').isIn(['ADMIN_ROLE','USER_ROLE']),
+        check("rol").custom(esRoleValido),
+        validarCampos,
+    ],
+    usuariosPost
+);
 
 // DELETE
-router.delete("/:id",[
-    validarJWT,
-    // esAdminRole,
-    tieneRole('ADMIN_ROLE', 'VENTAS_ROL'),
-    check('id', 'No es un ID válido').isMongoId(),
-    check('id').custom( existeUsuarioPorId ),
-    validarCampos 
-], usuariosDelete);
+router.delete(
+    "/:id",
+    [
+        validarJWT,
+        // esAdminRole,
+        tieneRole("ADMIN_ROLE", "VENTAS_ROL"),
+        check("id", "No es un ID válido").isMongoId(),
+        check("id").custom(existeUsuarioPorId),
+        validarCampos,
+    ],
+    usuariosDelete
+);
 
 //Patch
 router.patch("/", usuariosPatch);
