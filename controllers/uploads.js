3eb37df0842a9bcca2c8ce9ff29064a29de2f996
Tanelo <tanello.mondaca@gmail.com>
2022-07-21 14:47:40 -0400

    191. borrar archivos del servidor

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,5 +1,7 @@
-const { Usuario, Producto} = require('../models');
+const path = require('path');
+const fs  = require('fs');
 
+const { Usuario, Producto} = require('../models');
 
 const { response } = require("express");
 const { subirArchivo } = require("../helpers/subir-archivo");
@@ -26,14 +28,14 @@ const actualizarImagen = async (req, res = response) => {
         case 'usuarios':
             modelo = await Usuario.findById(id);
             if( !modelo ){
-                return res.status(400).json({ msg: 'No existe usuario con ese did'});
+                return res.status(400).json({ msg: 'No existe usuario con ese id'});
             };
 
             break;
         case 'productos':
             modelo = await Producto.findById(id);
             if( !modelo ){
-                return res.status(400).json({ msg: 'No existe producto con ese did'});
+                return res.status(400).json({ msg: 'No existe producto con ese id'});
             };
 
             break;
@@ -42,8 +44,20 @@ const actualizarImagen = async (req, res = response) => {
             return res.status(500).json({ msg: 'Se me olvido validar esto'});
     }
 
+    // Limpiar imagenes previas
+    if( modelo.img ){
+        // Hay que borrar la imagen del servidor
+        const pathImagen = path.join(__dirname,'../uploads', coleccion, modelo.img);
+        if( fs.existsSync(pathImagen) ){
+            //Si la imagen existe en la coleccion debe ser borrada
+            fs.unlinkSync(pathImagen);
+        }
+    }
+
     const nombre =  await subirArchivo(req.files, undefined, coleccion);
     modelo.img = nombre;
+    
+
     
     res.json({modelo});
 }
